37c65fdc04c32ef88b94826da4a7043b694772fc
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-20 00:26:49 +0600

    visualize loded data

---
+++ src/components/country/Country.jsx
@@ -0,0 +1,12 @@
+import "./country.css";
+export default function Country({ country }) {
+  const { name, flags} = country;
+  return (
+    <div>
+      <div className="country">
+        <h3>Name : {name?.common}</h3>
+        <img className="wd" src={flags.png} alt="" />
+      </div>
+    </div>
+  );
+}
